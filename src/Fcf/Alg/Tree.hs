b9ff6eb5cc8fcfae12190bfc15d671f9031eccbb
gspia <splistoja@iniffum.fi>
2022-06-01 20:44:19 +0300

    Remove extra space and parenthesis at Sizes

--- src/Fcf/Alg/Tree.hs
+++ src/Fcf/Alg/Tree.hs
@@ -46,7 +46,7 @@ type instance Eval (Map f ('NodeF a (b ': bs))) = 'NodeF a (Eval (Map f (b ': bs
 
 -- | A function to transform a Tree into fixed structure that can be used
 -- by Cata and Ana.
--- 
+--
 -- See the implementation of 'Size' for an example.
 data TreeToFix :: Tree a -> Exp (Fix (TreeF a))
 type instance Eval (TreeToFix ('Node a '[])) = 'Fix ('NodeF a '[])
@@ -55,15 +55,15 @@ type instance Eval (TreeToFix ('Node a (b ': bs))) =
 
 --------------------------------------------------------------------------------
 
--- | Sum the nodes of TreeF containing Nats. 
--- 
+-- | Sum the nodes of TreeF containing Nats.
+--
 -- See the implementation of 'Fib' for an example.
 data SumNodesAlg :: Algebra (TreeF Nat) Nat
 type instance Eval (SumNodesAlg ('NodeF x '[]))       = x
 type instance Eval (SumNodesAlg ('NodeF x (b ': bs))) = x TL.+ (Eval (Sum (b ': bs)))
 
 -- | Count the nodes of TreeF. 
--- 
+--
 -- See the 'Size' for an example.
 data CountNodesAlg :: Algebra (TreeF a) Nat
 type instance Eval (CountNodesAlg ('NodeF x '[]))       = 1
@@ -71,11 +71,11 @@ type instance Eval (CountNodesAlg ('NodeF x (b ': bs))) = 1 TL.+ (Eval (Sum (b '
 
 
 -- | Size of the Tree is the number of nodes in it.
--- 
+--
 -- __Example__
 --
 -- Size is defined as @ Cata CountNodesAlg =<< TreeToFix tr @
--- and can be used with the following. 
+-- and can be used with the following.
 --
 -- >>> data BuildNode :: Nat -> Exp (Nat,[Nat])
 -- >>> :{
@@ -113,7 +113,7 @@ type instance Eval (BuildFibTreeCoA n) =
 
 
 -- | Fibonaccis with Hylo, not efficient
--- 
+--
 -- __Example__
 --
 -- >>> :kind! Eval (FibHylo 10)
@@ -169,7 +169,7 @@ type instance Eval (FSum ('NodeF a (b ': bs))) = Eval (Sum (b ': bs))
 --                             'Fix ('AnnF '( 'NodeF 7 '[], 1))],
 --                        3))],
 --           7))
-type instance Eval (Sizes fx) = Eval (Synthesize ( ( (+) 1) <=< FSum) fx)
+type instance Eval (Sizes fx) = Eval (Synthesize ( (+) 1 <=< FSum) fx)
 data Sizes :: Fix f -> Exp (Ann f Nat)
 
 --------------------------------------------------------------------------------
