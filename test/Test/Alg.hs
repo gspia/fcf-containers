fd8590b0204a785506e7b9cb5aecce5ddc31ca61
gspia <splistoja@iniffum.fi>
2023-04-14 12:41:00 +0300

    Remove doctests from Fcf.Alg.Symbol
    Add respective tests to corresponding new test module.

--- test/Test/Alg.hs
+++ test/Test/Alg.hs
@@ -5,7 +5,8 @@ import           Test.Alg.List     as L (spec)
 import           Test.Alg.Morphism as M (spec)
 import           Test.Alg.Nat      as N (spec)
 import           Test.Alg.Other    as O (spec)
-import           Test.Alg.Sort     as S (spec)
+import           Test.Alg.Sort     as So (spec)
+import           Test.Alg.Symbol   as Sy (spec)
 import           Test.Hspec (describe, Spec)
 
 spec :: Spec
@@ -14,5 +15,6 @@ spec = describe "Alg" $ do
   M.spec
   N.spec
   O.spec
-  S.spec
+  So.spec
+  Sy.spec
 
