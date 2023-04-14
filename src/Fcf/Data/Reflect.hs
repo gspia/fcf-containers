6d59e66efaccc050e12b49f376255cb5a1981908
gspia <splistoja@iniffum.fi>
2023-04-14 08:28:42 +0300

    Remove commented imports

--- src/Fcf/Data/Reflect.hs
+++ src/Fcf/Data/Reflect.hs
@@ -33,7 +33,6 @@ import           Data.String (fromString, IsString)
 import           Data.Proxy
 import           Data.Typeable (Typeable, typeRep)
 import           Data.Kind (Type)
--- import qualified Data.Map.Strict as MS
 import qualified Data.Map as DM
 import qualified Data.IntMap.Strict as IMS
 import qualified Data.Set as S
@@ -41,7 +40,6 @@ import qualified Data.Set as S
 -- #endif
 import qualified Data.Tree as T
 
--- import qualified Fcf.Core as C (Eval)
 import qualified Fcf.Data.MapC as MC
 import qualified Fcf.Data.NatMap as NM
 import qualified Fcf.Data.Set as FS
@@ -52,15 +50,6 @@ import qualified Fcf.Data.Tree as FT
 
 --------------------------------------------------------------------------------
 
--- For the doctests:
-
--- $setup
--- >>> import qualified GHC.TypeLits as TL
--- >>> import           Fcf.Data.Nat
-
---------------------------------------------------------------------------------
-
-
 -- | Reflect a list of Nats
 --
 -- Note that you may also use the KnownVal methods given below.
@@ -70,7 +59,7 @@ import qualified Fcf.Data.Tree as FT
 --
 -- === __Example__
 --
--- >>> :{
+-- > :{
 -- afun :: forall n. (n ~ '[1,2,3,4]) => [Int]
 -- afun = natVals @n Proxy
 -- :}
