2c0ea01ae3bb934ba1666842e26cfb7b199631c2
gspia <splistoja@iniffum.fi>
2023-04-15 17:50:39 +0300

    fix typo on OldText module

--- src/Fcf/Data/OldText.hs
+++ src/Fcf/Data/OldText.hs
@@ -6,7 +6,7 @@
 {-# OPTIONS_GHC -Werror=incomplete-patterns #-}
 
 {-|
-Module      : Fcf.Data.Text
+Module      : Fcf.Data.OldText
 Description : Type-level Text data structure with methods
 Copyright   : (c) gspia 2020-
 License     : BSD
